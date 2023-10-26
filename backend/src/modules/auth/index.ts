d2dee72c151e326d89be5a7f323fb9a4f7b3d879
GenaRazmakhnin <gena.razmakhnin@gmail.com>
2023-10-26 12:19:42 +0200

    add client seed and ability to login as aidbox admin

--- backend/src/modules/auth/index.ts
+++ backend/src/modules/auth/index.ts
@@ -1,36 +1,42 @@
-import { randomUUID } from 'node:crypto'
-import { readFile } from 'node:fs/promises'
-import path from 'node:path'
+import { randomUUID } from 'node:crypto';
+import { readFile } from 'node:fs/promises';
+import path from 'node:path';
 
-import { BaseResponseResources } from 'aidbox'
-import { FastifyReply } from 'fastify/types/reply'
-import { FastifyRequest } from 'fastify/types/request'
-import { email, Input, object, string } from 'valibot'
+import { BaseResponseResources } from 'aidbox';
+import { FastifyReply } from 'fastify/types/reply';
+import { FastifyRequest } from 'fastify/types/request';
+import { email, Input, object, string } from 'valibot';
 
 const signupSchema = object({
   firstname: string(),
   lastname: string(),
   email: string([email()]),
-  password: string()
-})
-const resendConfirmationEmailSchema = object({ id: string() })
+  password: string(),
+});
+const resendConfirmationEmailSchema = object({ id: string() });
 export const endpoints = [
   {
     path: '/auth/$sign-up',
     method: 'post',
     authRequired: false,
     validation: signupSchema,
-    handler: async (request: FastifyRequest<{ Body: { data: Input<typeof signupSchema> } }>, reply: FastifyReply) => {
-      const { body: { data: body }, aidboxClient, appConfig } = request
+    handler: async (
+      request: FastifyRequest<{ Body: { data: Input<typeof signupSchema> } }>,
+      reply: FastifyReply,
+    ) => {
+      console.log('HERE');
+      const {
+        body: { data: body },
+        aidboxClient,
+        appConfig,
+      } = request;
       const userSearch = await aidboxClient
         .getHttpClient()
         .get(`User?.email=${body.email}`)
-        .json<BaseResponseResources<'User'>>()
+        .json<BaseResponseResources<'User'>>();
 
       if (userSearch.entry?.length) {
-        return reply
-          .status(400)
-          .send({ code: 'EMAIL_TAKEN' })
+        return reply.status(400).send({ code: 'EMAIL_TAKEN' });
       }
 
       const patient = await aidboxClient.resource.create('Patient', {
@@ -39,12 +45,12 @@ export const endpoints = [
           {
             use: 'usual',
             given: [body.firstname],
-            family: body.lastname
-          }
-        ]
-      })
-      const refLink = { id: patient.id, resourceType: patient.resourceType }
-      const uniqueCode = Buffer.from(randomUUID()).toString('base64')
+            family: body.lastname,
+          },
+        ],
+      });
+      const refLink = { id: patient.id, resourceType: patient.resourceType };
+      const uniqueCode = Buffer.from(randomUUID()).toString('base64');
       const user = await aidboxClient
         .getHttpClient()
         .post('User', {
@@ -57,23 +63,25 @@ export const endpoints = [
               roleName: 'patient',
               verified: false,
               firstEntry: true,
-              verificationCode: uniqueCode
+              verificationCode: uniqueCode,
             },
             name: {
               givenName: body.firstname,
-              familyName: body.lastname
-            }
-          }
+              familyName: body.lastname,
+            },
+          },
         })
-        .json<Record<string, any>>()
+        .json<Record<string, any>>();
 
-      const emailBody = (await readFile(path.resolve('./resources/email-verification.html')))
+      const emailBody = (
+        await readFile(path.resolve('./resources/email-verification.html'))
+      )
         .toString()
         .replace('%USERNAME%', `${body.firstname || ''} ${body.lastname || ''}`)
         .replace(
           '%REDIRECT_URL%',
-          `${appConfig.frontendAppUrl}/auth/sign-in?confirmation-code=${uniqueCode}`
-        )
+          `${appConfig.frontendAppUrl}/auth/sign-in?confirmation-code=${uniqueCode}`,
+        );
 
       await aidboxClient.getHttpClient().post('EmailNotification', {
         json: {
@@ -81,30 +89,34 @@ export const endpoints = [
           from: appConfig.mailgun.from,
           to: body.email,
           subject: 'Telemed: Email Verification',
-          body: emailBody
-        }
-      })
+          body: emailBody,
+        },
+      });
 
-      return reply.send({ status: true, id: user.id })
-    }
+      return reply.send({ status: true, id: user.id });
+    },
   },
   {
     path: '/auth/$resend-confirmation-email',
     method: 'post',
-    authRequired: true,
+    authRequired: false,
     validation: resendConfirmationEmailSchema,
     handler: async (
       request: FastifyRequest<{
         Body: { data: Input<typeof resendConfirmationEmailSchema> };
       }>,
-      reply: FastifyReply
+      reply: FastifyReply,
     ) => {
-      const { appConfig, body: { data: body }, aidboxClient } = request
+      const {
+        appConfig,
+        body: { data: body },
+        aidboxClient,
+      } = request;
 
       const user = await aidboxClient
         .getHttpClient()
         .get(`User/${body.id}`)
-        .json<Record<string, any>>()
+        .json<Record<string, any>>();
 
       const emailBody = (
         await readFile(path.resolve('./resources/email-verification.html'))
@@ -113,8 +125,8 @@ export const endpoints = [
         .replace('%USERNAME%', `${user.firstname || ''} ${user.lastname || ''}`)
         .replace(
           '%REDIRECT_URL%',
-          `${appConfig.frontendAppUrl}/auth/sign-in?confirmation-code=${user.data.verificationCode}`
-        )
+          `${appConfig.frontendAppUrl}/auth/sign-in?confirmation-code=${user.data.verificationCode}`,
+        );
 
       await aidboxClient.getHttpClient().post('EmailNotification', {
         json: {
@@ -122,113 +134,125 @@ export const endpoints = [
           from: appConfig.mailgun.from,
           to: body.id,
           subject: 'Telemed: Email Verification',
-          body: emailBody
-        }
-      })
+          body: emailBody,
+        },
+      });
 
-      return reply.send({ success: true })
-    }
+      return reply.send({ success: true });
+    },
   },
   {
     path: '/auth/$confirm-email',
     method: 'post',
-    authRequired: true,
-    handler: async (request: FastifyRequest<{ Body: { data: { code: string } } }>, reply: FastifyReply) => {
-      const { aidboxClient, body: { data: { code } } } = request
+    authRequired: false,
+    handler: async (
+      request: FastifyRequest<{ Body: { data: { code: string } } }>,
+      reply: FastifyReply,
+    ) => {
+      const {
+        aidboxClient,
+        body: {
+          data: { code },
+        },
+      } = request;
 
       const checkCode = await aidboxClient
         .getHttpClient()
         .get(`User?.data.verificationCode=${code}`)
-        .json<BaseResponseResources<'User'>>()
+        .json<BaseResponseResources<'User'>>();
 
       if (!checkCode.entry?.length) {
         return reply
           .status(400)
-          .send({ error: { message: 'Wrong verification code.' } })
+          .send({ error: { message: 'Wrong verification code.' } });
       }
 
-      const user = checkCode.entry[0].resource
+      const user = checkCode.entry[0].resource;
 
       const userResource = await aidboxClient
         .getHttpClient()
         .patch(`User/${user.id}`, {
           json: {
             data: {
-              verified: true
-            }
-          }
+              verified: true,
+            },
+          },
         })
-        .json<Record<string, any>>()
+        .json<Record<string, any>>();
 
-      return reply.send({ email: userResource.email })
-    }
+      return reply.send({ email: userResource.email });
+    },
   },
   {
     path: '/auth/$password-recovery-submit',
     method: 'post',
-    authRequired: true,
+    authRequired: false,
     handler: async (
       request: FastifyRequest<{
         Body: { data: { password: string; code: string } };
       }>,
-      reply: FastifyReply
+      reply: FastifyReply,
     ) => {
       const {
         aidboxClient,
         body: {
-          data: { password, code }
-        }
-      } = request
+          data: { password, code },
+        },
+      } = request;
 
       const userRaw = await aidboxClient
         .getHttpClient()
         .get(`User?.data.resetPasswordCode=${code}`)
-        .json<BaseResponseResources<'User'>>()
+        .json<BaseResponseResources<'User'>>();
 
       if (!userRaw.entry?.length) {
-        return reply.send({ error: { message: 'Wrong recovery code.' } })
+        return reply.send({ error: { message: 'Wrong recovery code.' } });
       }
-      const user = userRaw.entry[0].resource
+      const user = userRaw.entry[0].resource;
 
       await aidboxClient.getHttpClient().patch(`User/${user.id}`, {
         json: {
-          password
-        }
-      })
+          password,
+        },
+      });
 
-      return reply.send({ email: user.email })
-    }
+      return reply.send({ email: user.email });
+    },
   },
   {
     path: '/auth/$password-recovery',
     method: 'post',
-    authRequired: true,
+    authRequired: false,
     handler: async (
       request: FastifyRequest<{
         Body: { data: { email: string } };
       }>,
-      reply: FastifyReply
+      reply: FastifyReply,
     ) => {
-      const { aidboxClient, appConfig, body: { data: { email } } } = request
-      console.log(1)
+      const {
+        aidboxClient,
+        appConfig,
+        body: {
+          data: { email },
+        },
+      } = request;
+      console.log(1);
       const userRaw = await aidboxClient
         .getHttpClient()
         .get(`User?.email=${email}`)
-        .json<BaseResponseResources<'User'>>()
+        .json<BaseResponseResources<'User'>>();
 
       if (!userRaw.entry?.length) {
-        return reply
-          .status(404)
-          .send({ code: 'EMAIL_NOT_FOUND' })
+        return reply.status(404).send({ code: 'EMAIL_NOT_FOUND' });
       }
 
-      const uniqueCode = Buffer.from(randomUUID()).toString('base64')
+      const uniqueCode = Buffer.from(randomUUID()).toString('base64');
 
-      const user = userRaw.entry[0].resource
+      const user = userRaw.entry[0].resource;
 
       await aidboxClient.getHttpClient().patch(`User/${user.id}`, {
-        json: { data: { resetPasswordCode: uniqueCode } }
-      })
+        json: { data: { resetPasswordCode: uniqueCode } },
+      });
 
       const emailBody = (
         await readFile(path.resolve('./resources/password-recovery.html'))
@@ -236,8 +260,8 @@ export const endpoints = [
         .toString()
         .replace(
           '%RESET_PASSWORD_URL%',
-          `${appConfig.frontendAppUrl}/auth/reset-password?reset-password-code=${uniqueCode}`
-        )
+          `${appConfig.frontendAppUrl}/auth/reset-password?reset-password-code=${uniqueCode}`,
+        );
 
       await aidboxClient.getHttpClient().post('EmailNotification', {
         json: {
@@ -245,11 +269,11 @@ export const endpoints = [
           to: email,
           from: appConfig.mailgun.from,
           subject: 'Telemed: Password Recovery',
-          body: emailBody
-        }
-      })
+          body: emailBody,
+        },
+      });
 
-      return reply.send({ status: true })
-    }
-  }
-]
+      return reply.send({ status: true });
+    },
+  },
+];
--- frontend/src/service/aidbox.ts
+++ frontend/src/service/aidbox.ts
@@ -1,15 +1,15 @@
-import { Client, E, R } from 'aidbox'
-export { E, R }
+import { Client, E, R } from 'aidbox';
+export { E, R };
 
 export const box = new Client('http://127.0.0.1:8888', {
   auth: {
     method: 'resource-owner',
-    client: { id: 'root', secret: 'secret' },
+    client: { id: 'frontend', secret: 'secret' },
     storage: {
       get: () => localStorage.getItem('TOKEN'),
-      set: (token: string) => localStorage.setItem('TOKEN', token)
-    }
-  }
-})
+      set: (token: string) => localStorage.setItem('TOKEN', token),
+    },
+  },
+});
 
-export const http = box.HTTPClient()
+export const http = box.HTTPClient();
